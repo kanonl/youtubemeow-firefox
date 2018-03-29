0912de22f85aa67daf1f515b65ddec75bf7f9cc5
Kanon Lo <klo@jsls.com>
2018-03-29 15:26:23 -0400

    Refactored the onclick listener into the menu properties since Firefox does not support event pages.

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,35 +1,24 @@
 (function () {
     "use strict";
 
-    const menuItemId = browser.runtime.id;
-
-    const contextMenus = () => {
-        let createProperties = {
-            id: menuItemId,
-            title: "Search YouTube for \"%s\"",
-            contexts: ["selection"]
-        };
-
-        browser.menus.create(createProperties, () => {
-            if (browser.runtime.lastError) console.error(browser.runtime.lastError.message);
-        });
-    };
-
-    browser.runtime.onInstalled.addListener(event => contextMenus());
-
-    browser.runtime.onStartup.addListener(event => contextMenus());
-
-    browser.menus.onClicked.addListener((info, tab) => {
-        if (info.menuItemId === menuItemId) {
+    const createProperties = {
+        title: "Search YouTube for \"%s\"",
+        contexts: ["selection"],
+        onclick: (info, tab) => {
             let url = new URL("https://www.youtube.com");
             url.pathname = "/results";
             url.search = new URLSearchParams({
-                "search_query": info.selectionText
+                "search_query": info.selectionText.trim()
             });
 
             browser.tabs.create({
-                url: url.toString()
+                url: url.toString(),
+                active: true
             });
         }
+    };
+
+    browser.menus.create(createProperties, () => {
+        if (browser.runtime.lastError) console.error(browser.runtime.lastError.message);
     });
 })();
